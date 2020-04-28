b06b4bfb6ac1e6c53f9c6d316d5121970375722f
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-27 21:46:59 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Eminem")
+Artist.create(name: "The Weeknd")
+Artist.create(name: "Jay-Z")
+Artist.create(name: "Kanye West")
+
+Song.create(title: "Lose Yourself", artist_id: 1)
+Song.create(title: "Blinding Lights", artist_id: 2)
+Song.create(title: "The Ruler's Back", artist_id: 3)
+Song.create(title: "Follow God", artist_id: 4)
+Song.create(title: "Like Toy Soldiers", artist_id: 1)
+Song.create(title: "The Hills", artist_id: 2)
+
